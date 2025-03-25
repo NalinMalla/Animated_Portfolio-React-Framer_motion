e78c0aaa78d1a97ee20cd87167d285c2d3b93ebd
NalinMalla <nalinsmalla@gmail.com>
2025-03-25 09:44:21 +0545

    Added services.

---
+++ src/services/Services.jsx
@@ -0,0 +1,116 @@
+import { motion, useInView } from "framer-motion";
+
+import "./services.scss";
+import { useRef } from "react";
+
+const variants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef()
+
+  const isInView = useInView(ref, {margin: "-100px"})
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // whileInView="animate"  //starts the animation when the element comes to view port
+      ref={ref}
+      animate={isInView && "animate"}  //same as whileInView but gives more control over when the animation starts i.e. a 100px before it reaches viewport
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow
+          <br />
+          and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{color: "orange"}}>Unique</motion.b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{color: "orange"}}>For Your</motion.b> Business.
+          </h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laborum
+            architecto voluptate explicabo illum rerum! Libero voluptate eum
+            dolores, officia explicabo nesciunt nemo quod tenetur atque. Debitis
+            odio repellat quod!
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laborum
+            architecto voluptate explicabo illum rerum! Libero voluptate eum
+            dolores, officia explicabo nesciunt nemo quod tenetur atque. Debitis
+            odio repellat quod!
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laborum
+            architecto voluptate explicabo illum rerum! Libero voluptate eum
+            dolores, officia explicabo nesciunt nemo quod tenetur atque. Debitis
+            odio repellat quod!
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laborum
+            architecto voluptate explicabo illum rerum! Libero voluptate eum
+            dolores, officia explicabo nesciunt nemo quod tenetur atque. Debitis
+            odio repellat quod!
+          </p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
